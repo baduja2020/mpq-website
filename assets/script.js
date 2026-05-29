13316cc73a9cca2261fa24f58c30e6d8be04a74d
baduja2020 <bajulmuhammad@gmail.com>
2026-05-30 02:32:25 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -265,3 +265,13 @@ function formatTanggal(value) {
 }
 
 document.addEventListener("DOMContentLoaded", loadPengumuman);
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menuToggle");
+  const mainNav = document.getElementById("mainNav");
+
+  if (menuToggle && mainNav) {
+    menuToggle.addEventListener("click", function () {
+      mainNav.classList.toggle("show");
+    });
+  }
+});
